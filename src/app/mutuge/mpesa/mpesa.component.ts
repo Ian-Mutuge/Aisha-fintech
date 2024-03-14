763865fa6964efe0d49feb7b48e6acbd72a45517
Ian-Mutuge <99174650+Ian-Mutuge@users.noreply.github.com>
2024-03-14 15:51:34 +0300

    mpesa assignment

--- src/app/mutuge/mpesa/mpesa.component.ts
+++ src/app/mutuge/mpesa/mpesa.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mpesa',
-  templateUrl: './mpesa.component.php',
+  templateUrl: './mpesa.component.html',
   styleUrl: './mpesa.component.css'
 })
 export class MpesaComponent {
-  constructor(){}
+  constructor(){
+    
+  }
 }
